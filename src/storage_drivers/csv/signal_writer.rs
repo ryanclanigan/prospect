02d635ca5a026a9a98c665417666a07b61934750
Ryan Lanigan <ryan.lanigan@seeq.com>
2020-01-29 15:26:32 -0800

    Samples must now be ordered

--- src/storage_drivers/csv/signal_writer.rs
+++ src/storage_drivers/csv/signal_writer.rs
@@ -1,4 +1,5 @@
 use super::sample_record::SampleRecord;
+use crate::primitives::sample::Sample;
 use crate::primitives::signal::Signal;
 use anyhow::Result;
 use csv::Writer;
@@ -7,11 +8,25 @@ use std::path::Path;
 pub struct SignalWriter;
 
 impl SignalWriter {
+    const SAMPLE_WRITE_MESSAGE: &'static str =
+        "Samples are out of order or have duplicate keys. Please make sure your data is accurate";
+
     pub fn write_signal(&self, signal: &mut Signal, file: &Path) -> Result<()> {
         let mut writer = Writer::from_path(file)?;
-        for sample in signal.get_samples() {
+        let mut iter = signal.get_samples().iter();
+        let first_sample = match iter.next() {
+            Some(s) => s,
+            None => return Err(anyhow!("Signal does not have any samples")),
+        };
+        writer.serialize(SampleRecord::from_sample(first_sample))?;
+        let mut last_sample: Sample = first_sample.clone();
+        for sample in iter {
+            if !last_sample.is_keyed_before(sample) {
+                return Err(anyhow!(Self::SAMPLE_WRITE_MESSAGE));
+            }
             let record = SampleRecord::from_sample(sample);
             writer.serialize(record)?;
+            last_sample = sample.clone();
         }
 
         writer.flush()?;
